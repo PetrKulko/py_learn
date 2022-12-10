78e8d876cde1ea025af04cf4cf97179ae7ada360
Peter_Kulko <petrokulko@gmail.com>
2022-12-10 19:13:24 +0200

    variables and types of data

---
+++ data_types.py
@@ -0,0 +1,25 @@
+# Data types
+
+# Integer (int)
+int_items = 3, 12, 24
+
+# Floating points (float)
+float_items = 4.02, -0.23, 1.2
+
+# String (str)
+str_items = "Hello", "How are you?"
+
+# List (list)
+list_items = ["Hello", 12, 98.02]
+
+# Dictionary (dict) - "key-values"
+dict_items = {"luckyNumber": "444", 'name': 'Peter'}
+
+# Tuple (tup) immutable order objects
+tuple_items = ("Hello", 12, 54.2, 12)
+
+# Set unordered, unique objects
+set_items = ("Hello", 12, 0.2)
+
+# Boolean (bool)
+bool_item = 'false'
---
+++ vars.py
@@ -0,0 +1,15 @@
+# x = 5
+# y = 2
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x // y)
+# print(x ** y)
+# print(x % y)
+
+credit = 1000
+credit_rate = 10
+number_of_years = 5
+final_sum = credit + credit / 100 * credit_rate * number_of_years
+print(final_sum)
