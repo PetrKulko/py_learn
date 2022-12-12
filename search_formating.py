6538ee052d2595a92d864c01af6c9d7774f278a1
Peter_Kulko <petrokulko@gmail.com>
2022-12-12 19:10:44 +0200

    list

---
+++ search_formating.py
@@ -0,0 +1,34 @@
+# print(1 + 1)
+# print("1" + "1")
+# age = 23
+# print("Peter is " + str(age) + ' years old.')
+# print("Peter is " + str(23) + ' years old.')
+
+# name = "Peter"
+# age = 27
+# name_and_age = "My name is {0}. I`m {1} years old.".format(name, age)
+#
+# print(name_and_age)
+# name_and_age2 = "My name is {0}. I`m {1} years old.".format("Jack", 23)
+# print(name_and_age2)
+#
+# name_and_age3 = "My name is {}. I`m {} years old.".format("Jack", 23)
+# print(name_and_age3)
+#
+# week_days = "There are 7 days a week: {sd}, {mo}, {th}, {wd}, {td}, {fr}, {st}".format(
+#     mo='Monday',
+#     th='Tuesday',
+#     wd='Wednesday',
+#     td='Thursday',
+#     fr='Friday',
+#     st='Saturday',
+#     sd='Sunday'
+# )
+# print(week_days)
+
+name = "Jack"
+age = 23
+# name_and_age = f"My name is {name}. I`m {age} years old."
+# print(name_and_age)
+
+print("My name is %s. I`m %d years old." % (name, age))
